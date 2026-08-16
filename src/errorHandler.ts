ba06807484e9b378d9153dd506cc5a7fce892a86
Vikram <vikram0x00@proton.me>
2026-08-16 16:38:27 +0530

    Complete Button Bot with all the necessary features and proper error handling function

--- src/errorHandler.ts
+++ src/errorHandler.ts
@@ -1,7 +1,16 @@
 import { type ErrorHandler } from "grammy";
 
-const errorHandler: ErrorHandler = (error)=>{
-	console.log("BOT ERROR", error.message, error.stack);
+const errorHandler: ErrorHandler = async (error)=>{
+	if(error.message === `GrammyError in middleware: Call to 'editMessageReplyMarkup' failed! (400: Bad Request: message is not modified: specified new message content and reply markup are exactly the same as a current content and reply markup of the message)`){
+		await error.ctx.reply("No Inline Buttons found in the given message");
+	}
+	else if(error.message === `GrammyError in middleware: Call to 'editMessageReplyMarkup' failed! (400: Bad Request: MESSAGE_ID_INVALID)`){
+		await error.ctx.reply("The Message with the provided message identifier not found in the provided chat.");
+	}
+	else if(error.message === `GrammyError in middleware: Call to 'editMessageReplyMarkup' failed! (400: Bad Request: message identifier is not specified)`){
+		await error.ctx.reply("The Message with the provided message identifier not found in the provided chat. \nOther reasons might be that there are no inline buttons on the message provided");
+	}
+	console.log(error.message);
 }
 
 export default errorHandler;
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ interface SessionData {
 	state: "awaiting_link" | "awaiting_buttons_data";
 	chatId: number | string | undefined;
 	messageId: number | undefined;
+	parsedButtons?: any | undefined;
 }
 
 type BotContext = Context & SessionFlavor<SessionData>;
@@ -35,6 +36,47 @@ bot.command("start", async (ctx)=>{
 	await ctx.reply("Hello");
 });
 
+bot.command("cancel", async (ctx)=>{
+	if(!ctx.session.chatId || !ctx.session.messageId){
+		await ctx.reply("No Operation is being carried out. Nothing to cancel");
+		return;
+	}
+	await ctx.reply("Operation Cancelled!");
+	ctx.session.state = "awaiting_link";
+});
+
+bot.callbackQuery("remove", async (ctx)=>{
+	await ctx.answerCallbackQuery();
+	await ctx.editMessageReplyMarkup({
+		reply_markup: {
+			inline_keyboard: []
+		}
+	});
+	await bot.api.editMessageReplyMarkup(ctx.session.chatId!, ctx.session.messageId!, {
+		reply_markup: {
+			inline_keyboard: []
+		}
+	});
+	await ctx.reply("Successfully Removed Buttons");
+	ctx.session = { state: "awaiting_link", chatId: undefined, messageId: undefined }
+});
+
+bot.callbackQuery("submit", async (ctx)=>{
+	await ctx.answerCallbackQuery();
+	await ctx.editMessageReplyMarkup({
+		reply_markup: {
+			inline_keyboard: []
+		}
+	});
+	await bot.api.editMessageReplyMarkup(ctx.session.chatId!, ctx.session.messageId!, {
+		reply_markup: {
+			inline_keyboard: ctx.session.parsedButtons
+		}
+	});
+	await ctx.reply("Buttons Edited Successfully");
+	ctx.session = { state: "awaiting_link", chatId: undefined, messageId: undefined, parsedButtons: undefined };
+});
+
 bot.on("message:text", async (ctx)=>{
 	/**
 	 * Step 1 - User Sends a Telegram Message Link to the Bot
@@ -46,12 +88,12 @@ bot.on("message:text", async (ctx)=>{
 	 */
 	const messageText = ctx.message.text;
 	if(ctx.session.state === "awaiting_link"){
-		if(!messageText.startsWith("https://t.me/c/")){
+		if(!messageText.startsWith("https://t.me/c/") || !messageText.startsWith("https://t.me/")){
 			await ctx.reply("Invalid Message Link Sent");
 			return;
 		}
 		const arr = messageText.split("/");
-		await ctx.reply("Great! Please Send your Buttons in a Format like this\n\n[Text]:[URL]\nExample: <code>Button:https://google.com</code>", {
+		await ctx.reply("Great! Please Send your Buttons in a Format like this\n\nExample:\n<code>Button 1:https://google.com</code>\n<code>Button 2:https://reddit.com</code>", {
 			parse_mode: "HTML",
 			reply_markup: {
 				inline_keyboard: [
@@ -59,9 +101,18 @@ bot.on("message:text", async (ctx)=>{
 				]
 			}
 		});
-		ctx.session = { state: "awaiting_buttons_data", chatId: "-100" + arr[4], messageId: Number(arr[5]) }
+		if(messageText.startsWith("https://t.me/c/")){
+			ctx.session = { state: "awaiting_buttons_data", chatId: "-100" + arr[4], messageId: Number(arr[5]) }
+		}
+		else if(messageText.startsWith("https://t.me/")){
+			ctx.session = { state: "awaiting_buttons_data", chatId: arr[3], messageId: Number(arr[4]) }
+		}
 	}
 	else if(ctx.session.state === "awaiting_buttons_data"){
+		if(ctx.message.text.startsWith("https://t.me/")){
+			await ctx.reply("There is an operation running. Please /cancel it or send the buttons in the required format.");
+			return;
+		}
 		const buttons = ctx.message.text.split("\n");
 		let parsedBtns;
 		try {
@@ -73,23 +124,12 @@ bot.on("message:text", async (ctx)=>{
 		await ctx.reply("Here is a Preview of your button(s):", {
 			reply_markup: {
 				// @ts-ignore
-				inline_keyboard: parsedBtns.concat([{ text: "Submit", callback_data: "submit" }])
+				inline_keyboard: parsedBtns.concat([[{ text: "Submit", callback_data: "submit" }]])
 			}
 		});
+		ctx.session.parsedButtons = parsedBtns;
 	}
 });
-
-bot.callbackQuery("remove", async (ctx)=>{
-	console.log(ctx.session);
-	await ctx.answerCallbackQuery();
-	await bot.api.editMessageReplyMarkup(ctx.session.chatId!, ctx.session.messageId!, {
-		reply_markup: {
-			inline_keyboard: []
-		}
-	});
-});
-
-// bot.callbackQuery("submit");
 
 bot.start();
 
